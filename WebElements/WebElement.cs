90f0e055daf0da68218e3700341160695e12a57c
Sergey Valyushko <sergeyvalyushko@coherentsolutions.com>
2021-01-30 19:36:28 +0300

    commit_1

---
+++ WebElements/WebElement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class WebElement
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string name { get; set; }
+        public WebElement (int x, int y, string name)
+        {
+            this.X = x;
+            this.Y = y;
+            this.name = name;
+        }
+    }
+}
